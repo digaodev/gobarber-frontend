c96d1e4f6b285657f4037a4c31e1e77c7cf9edc5
digaodev <rodrigo.rb.dev@gmail.com>
2019-09-18 13:31:20 -0300

    feature: add styled-components. Add global styles and base layouts. Remove babel-eslint due to cra conflict.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
+import GlobalStyles from './styles/global';
 
 import history from './services/history';
 import Routes from './routes';
@@ -9,6 +10,7 @@ import Routes from './routes';
 function App() {
   return (
     <Router history={history}>
+      <GlobalStyles />
       <Routes />
     </Router>
   );
